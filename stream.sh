70be4bd581f44190d0125fa65c9934467cf3bd79
rob <rob@clabs.cc>
2013-09-22 17:39:10 +0200

    removed some \s chars

--- stream.sh
+++ stream.sh
@@ -12,34 +12,34 @@ AIRCAM_CAPS="video/x-raw-yuv, width=1280, height=720, framerate=$FPS/1"
 AUDIO_CAPS="audio/x-raw-int"
 
 # Sources
-		SRC1="rtspsrc location=$AIRCAM1 latency=0 ! decodebin ! ffmpegcolorspace ! $AIRCAM_CAPS"
-		#SRC1="videotestsrc pattern=2"
+SRC1="rtspsrc location=$AIRCAM1 latency=0 ! decodebin ! ffmpegcolorspace ! $AIRCAM_CAPS"
+#SRC1="videotestsrc pattern=2"
 
-		SRC2="rtspsrc location=$AIRCAM2 latency=0 ! decodebin ! ffmpegcolorspace ! $AIRCAM_CAPS"
-		#SRC2="videotestsrc pattern=2"
+SRC2="rtspsrc location=$AIRCAM2 latency=0 ! decodebin ! ffmpegcolorspace ! $AIRCAM_CAPS"
+#SRC2="videotestsrc pattern=2"
 
-		SRC3="rtspsrc location=$AIRCAM3 latency=0 ! decodebin ! ffmpegcolorspace ! $AIRCAM_CAPS"
-		#oOSRC3="videotestsrc pattern=2"
+SRC3="rtspsrc location=$AIRCAM3 latency=0 ! decodebin ! ffmpegcolorspace ! $AIRCAM_CAPS"
+#oOSRC3="videotestsrc pattern=2"
 
-		SRC4="multifilesrc location=\"layer.png\" caps=\"image/png,framerate=$FPS/1\" ! pngdec ! imagefreeze ! ffmpegcolorspace ! video/x-raw-yuv,format=(fourcc)AYUV,framerate=$FPS/1"
-		AUDIO="autoaudiosrc ! audioconvert ! $AUDIO_CAPS"
+SRC4="multifilesrc location=\"layer.png\" caps=\"image/png,framerate=$FPS/1\" ! pngdec ! imagefreeze ! ffmpegcolorspace ! video/x-raw-yuv,format=(fourcc)AYUV,framerate=$FPS/1"
+AUDIO="autoaudiosrc ! audioconvert ! $AUDIO_CAPS"
 
 # Justin Kram
-	STREAM_KEY="live_" 
-	RTMP_SERVER_URL="rtmp://live.justin.tv/app"
-	JUSTIN="\"$RTMP_SERVER_URL/$STREAM_KEY flashver=FME/2.5\20(compatible;\20FMSc\201.0)\""
-	
+STREAM_KEY="live_" 
+RTMP_SERVER_URL="rtmp://live.justin.tv/app"
+JUSTIN="\"$RTMP_SERVER_URL/$STREAM_KEY flashver=FME/2.5\20(compatible;\20FMSc\201.0)\""
+
 # Steaming Output
-	ENCODER="x264enc pass=pass1 threads=0 bitrate=$VIDEO_BITRATE byte-stream=true tune=zerolatency speed-preset=ultrafast"
-	RTMPSINK="rtmpsink location=$JUSTIN async=true"
-	TCPSINK="tcpserversink host=172.23.1.241 port=1234"
-	QUEUE="queue leaky=1"
+ENCODER="x264enc pass=pass1 threads=0 bitrate=$VIDEO_BITRATE byte-stream=true tune=zerolatency speed-preset=ultrafast"
+RTMPSINK="rtmpsink location=$JUSTIN async=true"
+TCPSINK="tcpserversink host=172.23.1.241 port=1234"
+QUEUE="queue leaky=1"
 
 gst-launch-0.10 \
 	videomixer name=mix \
-			sink_0::zorder=1 sink_0::xpos=0    sink_0::ypos=0 \
-			sink_1::zorder=3 sink_1::xpos=30  sink_1::ypos=226 \
-			sink_2::zorder=4 sink_2::xpos=30  sink_2::ypos=444 \
+			sink_0::zorder=1 sink_0::xpos=0  sink_0::ypos=0 \
+			sink_1::zorder=3 sink_1::xpos=30 sink_1::ypos=226 \
+			sink_2::zorder=4 sink_2::xpos=30 sink_2::ypos=444 \
 			sink_3::zorder=2 sink_3::xpos=0  sink_3::ypos=0 \
 	! ffmpegcolorspace ! videorate ! $ENCODER ! flvmux name=mux streamable=true ! tee name=t \
 		t. ! $QUEUE ! $TCPSINK \
